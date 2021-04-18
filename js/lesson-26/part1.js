c5c2edc4fa4988abe18aec28ed5c28a6cf7a3757
Yurii <ukucher@gmail.com>
2021-04-18 16:47:22 +0300

    Ready for coderiwiev

---
+++ js/lesson-26/part1.js
@@ -0,0 +1,37 @@
+`use strict`;
+
+const vehicle = {
+    manufacturer: `AUDI`,
+    model: `A80`,
+    yearOfManufacture: `2020`,
+    averagSpeed: 50,
+    powerReserve: 600,
+    displayAllAbout() {
+        for (let key in vehicle) {
+            if (typeof vehicle[key] !== 'function') {
+                console.log(`${key} : ${this[key]}`)
+            }
+        }
+    },
+
+    getTimeToTripWithoutRest(dist) {
+        return dist / this.averagSpeed;
+    },
+
+    getTimeToTrip(dist) {
+        timeWithoutRest = this.getTimeToTripWithoutRest(dist);
+        restTime = (timeWithoutRest % 4 !== 0) ? Math.trunc(timeWithoutRest / 4) : Math.trunc(timeWithoutRest / 4) - 1;
+        return timeWithoutRest + restTime;
+    },
+}
+
+vehicle.displayAllAbout(); // Функція для виводу інформації про автомобіль;
+
+distance = 401; // Задаємо дистанцію в кілометрах
+
+console.log(vehicle.getTimeToTripWithoutRest(distance)); // Час поїздки без зупинок
+
+console.log(`На ${vehicle.manufacturer} ${vehicle.model} можна подолати ${distance} км приблизно за ${vehicle.getTimeToTripWithoutRest(distance)} годин(и) без зупинок на відпочинок.`); // Розгорнутий вивід про поїздку без зупинок
+
+console.log(vehicle.getTimeToTrip(distance)); // Час поїздки з зупинками кожні 4 години
+console.log(`На ${vehicle.manufacturer} ${vehicle.model} можна подолати ${distance} км приблизно за ${vehicle.getTimeToTrip(distance)} годин(и) з 60-ти хвилинними зупинками кожні 4 години.`); // Розгорнутий вивід про поїздку з зупинками
